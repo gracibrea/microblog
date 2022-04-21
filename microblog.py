f81a79aec7b3f7b5a19cceb71896f5e4c212acc7
Claudia Kruger <claudiagracieli@gmail.com>
2022-04-21 09:47:15 -0300

    Alterações para publicação no Heroku

--- microblog.py
+++ microblog.py
@@ -1,6 +1,8 @@
 from app import app
 import os
-
+"""
+se for o main criar a variável PORT para que ele recupere no sistema operacional a porta local onde o servidor estará rodando. Se o Heroku não setar nenhuma, ele utilizará a porta 5000
+"""
 if __name__== 'main':
     port = int(os.getenv('PORT'), '5000')
     app.run(HOST='0.0.0.0', port = port)
